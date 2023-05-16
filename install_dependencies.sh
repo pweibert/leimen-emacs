56a32e01e9fc73efaef286f1944189adf30863b7
Paul Weibert <Paul.Weibert@kgu.de>
2023-05-16 13:44:36 +0200

    Add vterm dependencies to install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,3 +5,6 @@ python3 -m pip install python-language-server
 # lsp mode python debugging
 python3 -m pip install debugpy
 
+# requirements to build vterm
+sudo apt install libtool
+sudo apt install libtool-bin
