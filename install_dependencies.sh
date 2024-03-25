75da8ed3d1f49a2e52f6474d85ab1bee33c5a59d
Paul Weibert <Paul.Weibert@kgu.de>
2024-03-25 18:56:02 +0100

    Add installation of python flake8 dependency needed for pylsp

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,7 @@
 # lsp mode python language support
 python3 -m pip install python-language-server
 python3 -m pip install python-lsp-server
-
+python3 -m pip install flake8
 # lsp mode python debugging
 python3 -m pip install debugpy
 
