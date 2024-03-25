8d664a12fa39c1ac71cf2134f755cb95d5c83113
Paul Weibert <Paul.Weibert@kgu.de>
2024-03-25 18:57:25 +0100

    Formatting

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+
 # lsp mode python language support
 python3 -m pip install python-language-server
 python3 -m pip install python-lsp-server
 python3 -m pip install flake8
+
 # lsp mode python debugging
 python3 -m pip install debugpy
 
