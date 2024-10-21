b6b84eb70857df713a20e3d9f974e54a17ad601d
Paul Weibert <Paul.Weibert@kgu.de>
2024-10-21 18:23:45 +0200

    Fixed aissist generation
    - Also add ai gen start and end markers befor inserting ai generated code

--- include/aissist.el
+++ include/aissist.el
@@ -2,45 +2,12 @@
 (setq ollama-models-registry '())
 (setq ollama-llm-providers '())
 
-(defun build-ollama-llm-providers ()
-  "Builds a list of ollama providers.
-
-   The list has elements in this form (provider-name (make-llm-ollama ...)) and thus can be directly passed to ellama.
-   "
-  (let ((ollama-llm-providers '()))  ;; Locally declared variable with `let`
-    (dolist (pair ollama-models-registry)
-      (let* ((el_identifier (car pair))
-             (ollama_model_reference (cdr pair))
-             (provider-name (concat el_identifier "-provider"))
-             (provider-instance (make-llm-ollama :embedding-model ollama_model_reference
-                                                 :chat-model ollama_model_reference
-                                                 :default-chat-temperature 0.1)))
-        ;; Modifying the local `ollama-llm-providers` list (created by `let`)
-        (setq ollama-llm-providers (add-to-list 'ollama-llm-providers (cons provider-name provider-instance) t))
-        (message "++ build el_identifier %s" el_identifier) ))
-    ;; Returning the local variable
-    ollama-llm-providers))
-
 (require 'llm)
 (defgroup aissist nil
   "Large language model code completion."
   :prefix "aissist-"
   :group 'editing)
 
-(defcustom aissist-fast-model
-  "zephyr:latest"
-  "Name of the ollama model for faster code completions."
-  :type 'string
-  :group 'ai-assist)
-
-(defcustom aissist-accurate-model
-  ;;  "wizardcoder:33b-v1.1-q4_1"
-  "wizardcoder:33b-v1.1-q4_1"
-  "Name of the ollama model for accurate code completions."
-  :type 'string
-  :group 'aissist)
-
-(setq current-llm-request "")
 
 (defun kill-processes-by-wildcard (pattern)
   "Kill all processes whose name matches the given PATTERN, and suppress save prompts."
@@ -66,72 +33,21 @@
 ;; Allow to terminate ai streaming interactively
 (global-set-key (kbd "C-g") 'keyboard-quit-mod)
 
-
-(defun aissist-complete-generic (llm-model)
-  "Run the ollama LLM providing predefined prompt and current buffer context for completion."
+(defun add-ai-gen-markers ()
   (interactive)
-  (let* ((spot (point))
-         (inhibit-quit t)
-         (curfile (buffer-file-name))
-         (cash (concat curfile ".cache"))
-         (hist (concat curfile ".prompt"))
-         (lang (file-name-extension curfile))
-         (current-buffer (current-buffer))
-         ;; extract current line, to left of caret
-         ;; and the previous line, to give the llm
-         (code (save-excursion
-                 (beginning-of-buffer)
-                 (buffer-substring-no-properties (point) spot)))
+  (save-excursion
+  (deactivate-mark)
+  (insert "AI-GEN-START")
+  (comment-line 1)
+  (newline)
+  (insert "AI-GEN-END")
+  (newline)
+  (forward-line -1)
+  (comment-line 1)
+  )
+  (next-line 1))
 
-         ;; create new prompt for this interaction
-         (system "\
-You are an Emacs code generator. \
-Writing comments is forbidden. \
-Writing test code is forbidden. \
-Writing explanations is forbidden. ")
-         (prompt (format
-                  "[INST]%sGenerate %s code:[/INST]\n```%s\n%s"
-                  (if (file-exists-p cash) "" system) lang lang code)))
-
-    ;; append prompt for current interaction to the big old prompt
-    (write-region prompt nil hist nil 'silent)
-
-    ;; call ollama model using `llm-call` with the appropriate model and parameters
-    ;; (with-local-quit
-    ;;   (apply 'llm- llm-model
-    ;;          :input prompt
-    ;;          :params (append llm-params (list
-    ;;                                      :cache cash
-    ;;                                      :silent t
-    ;;                                      :max-tokens 2048
-    ;;                                      :temperature 0.0
-    ;;                                      :n 1))))
-    
-    (setq current-llm-request (llm-chat-streaming-to-point llm-model prompt current-buffer current-point nil))
-
-    (message ">> post call ")
-    ;; get rid of most markdown syntax
-    (let ((end (point)))
-      (save-excursion
-        (goto-char spot)
-        (while (search-forward "\\_" end t)
-          (backward-char)
-          (delete-backward-char 1 nil)
-          (setq end (- end 1)))
-        (goto-char spot)
-        (while (search-forward "```" end t)
-          (delete-backward-char 3 nil)
-          (setq end (- end 3))))
-
-      ;; append generated code to prompt
-      ;;(write-region spot end hist 'append 'silent)
-      )))
-
-(defun aissist-dummy-callback (result)
-  "Dummy callback that prints the RESULT argument."
-  (message "Aissist result: %s" result))
-
-(defun aissist-run-ollama-generic (ollama-model-id)
+(defun aissist-run-ollama-generic (ollama-model)
   "Run llm-chat-streaming-to-point with the aissist-accurate provider at the current point in the current buffer."
   (defvar-local llm-chat-streaming-prompt nil)
 
@@ -140,34 +56,39 @@ Writing explanations is forbidden. ")
          (curfile (buffer-file-name))
          (hist (concat curfile ".prompt"))
          (lang (file-name-extension curfile))
-
-         ;; extract current line, to left of caret
-         ;; and the previous line, to give the llm
-         (code (save-excursion(beginning-of-buffer)
-                 (buffer-substring-no-properties (point) spot)))
-         (prompt (format "\
-
+         (start (min (region-beginning) (region-end)))
+         (end (max (region-beginning) (region-end)))
+         (template_prompt "\
 language: %s.\n
 code:\n%s\n
 You are a emacs code generator.\n
 Explanations are forbidden!\n
-I only want the part to add to my script, not the whole script!\n
-Mark generated code beginning witt 'AI-GEN-START' and end with 'AI-GEN-END' comment." lang code))
-        (current-buffer (current-buffer))
-        (current-point (point))
-        (llm-model-provider (make-llm-ollama :embedding-model ollama-model-id :chat-model ollama-model-id :default-chat-temperature 0.1))
-        ) ;; Define a simple prompt
+I only want the part to add to my code, not the whole code!\n
+I want the result without markdown quotation please!")
+         ;; make sure we do not feed more text then is allowed by the maximum context
+         (min-start-position (save-excursion (backward-word (- (llm-chat-token-limit ollama-model) (llm-count-tokens ollama-model prompt))) (point)))
+         (code
+          (if (use-region-p)
+              ;; Region exists, store boundaries and print them
+              (save-excursion
+                (buffer-substring-no-properties start end))
+            (save-excursion
+              (buffer-substring-no-properties min-start-position (point)))
+            ))
+         (prompt (format template_prompt lang code))
+         ) ;; Define a simple prompt
 
-    (message "ai-assistent model: %s" ollama-model-id)
-    (message "ai-assistent prompt: \n%s:" prompt)
+    (message "aissist: prompt: %s" prompt)
+    (message "aissist: model provider: \n%s:" ollama-model)
+    (add-ai-gen-markers)
     ;; Call the llm-chat-streaming-to-point with aissist-accurate provider
-    (setq current-llm-request (llm-chat-streaming-to-point
-     llm-model-provider     ;; The provider (aissist-accurate)
+    ;;(sleep-for 0.1)
+    (llm-chat-streaming-to-point
+     ollama-model     ;; The provider
      (llm-make-chat-prompt prompt) ;; The prompt for code generation
-     current-buffer       ;; The current buffer
-     current-point        ;; The current point
-     'aissist-dummy-callback)))) ;; Dummy callback
-
+     (current-buffer)       ;; The current buffer
+     (point)        ;; The current point
+     nil))) ;; Dummy callback
 
 (defun build-ollama-llm-providers ()
   "Builds a list of ollama providers.
@@ -181,7 +102,7 @@ Mark generated code beginning witt 'AI-GEN-START' and end with 'AI-GEN-END' comm
              (provider-name (concat el_identifier "-provider"))
              (provider-instance (make-llm-ollama :embedding-model ollama_model_reference
                                                  :chat-model ollama_model_reference
-                                                 :default-chat-temperature 0.1)))
+                                                 :default-chat-temperature 0.9)))
         ;; Modifying the local `ollama-llm-providers` list (created by `let`)
         (setq ollama-llm-providers (add-to-list 'ollama-llm-providers (cons provider-name provider-instance) t))))
     ;; Returning the local variable
